89599dc3cd1530d002769a6239b9cace07ae2afe
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-03-07 15:01:26 -0500

    feat: :construction: añadir plantilla de telefono
    descripcion:
    - se añade el codigo de js para añadir la plantilla del formulario de telefono
    - se serpara la data de los formularios en archivos a parte

--- js/add.js
+++ js/add.js
@@ -1,42 +1,50 @@
-//segun lo que apa
-const formAddActive = [
-    {
-        value : ["active-code", "Codigo de la transaccion"], //Id que relaciona al input con el label y el texto
-        typeInput : "number",
-    },
-    {
-        value : ["active-name", "Nombre del activo"], //Id que relaciona al input con el label y el texto
-        typeInput : "text",
-    },
-    {
-        value : ["active-serial", "Numero del serial"], //Id que relaciona al input con el label y el texto
-        typeInput : "text",
-    },
-    {
-        value : ["active-value", "Valor Unitario"], //Id que relaciona al input con el label y el texto
-        typeInput : "number",
-    },
-    {
-        value : ["category-active", "Categoria del activo"], 
-        typeInput : "select",
-    },
-    {
-        value : ["active-type", "Tipo del activo"], 
-        typeInput : "select",
-    },
-    {
-        value : ["active-status", "Estado del activo"], 
-        typeInput : "select",
-    },
-    {
-        value : ["active-brand", "Marca del activo"], 
-        typeInput : "select",
-    },
-    {
-        value: ["add-active-id", "registrar Activo"],
-        typeInput : "submit",
-    }
-]
+//Importaciones
+import { activeInfo, personInfo } from "./dataForm.js";
+
+//crear telefonos
+let count = 0;
+
+
+function newPhone(){
+    //Se crea la etiqueta de formulario y se adjunta al contenedor
+    const container = document.createElement('section');
+    container.classList.add('container-form');
+    const form = document.createElement('form');
+    form.classList.add('register__form', 'phone__form');
+    container.appendChild(form);
+
+    form.innerHTML = `
+        <h2>Nuevo Telefono</h2>        
+        <div>
+            <!-- Numero de telefono-->
+            <label for="person-phone1">Numero de telefono: </label>
+            <input class="input__form" type="number" id="person-phone${count}" required>
+        </div>
+                        
+        <div>
+            <!-- Ubicacion-->
+            <label for="person-phone-ubicacion">Ubicacion: </label>
+            <input class="input__form" type="text" id="person-phone-ubicacion${count}" required>
+        </div>
+        <button class="add__info" id="add__phone${count}">
+            Añadir otro telefono
+            <i class='bx bx-plus'></i>
+        </button>
+    `
+    document.querySelector('main').appendChild(container);
+    count++;
+}
+newPhone();
+
+document.querySelectorAll('.add__info').forEach((button) =>{
+
+    button.addEventListener('click',(e)=> {
+        e.preventDefault();
+        newPhone()})
+})
+
+
+// Renderizar formularios
 function addForm(newForm, title){
     //se crea el contenedor del fomulario y se pone en el main
     const container = document.createElement('section');
@@ -57,16 +65,21 @@ function addForm(newForm, title){
     newForm.forEach((input)=>{
         switch (input.typeInput){
             //Rceibe numeros y texto
+            case 'email':
             case 'text':
             case 'number':
                 {
-                    console.log(input.typeInput)
                     const div = document.createElement('div');
                     //Lo que va adentro del div
+                    if (input.value[0].includes('phone')){
+                        console.log("Esto hace parte del formulario de telefono :D")
+                    }
+                     
                     div.innerHTML = `
                         <label for="${input.value[0]}">${input.value[1]}: </label>
-                        <input  class="input__form" type="${input.typeInput}" id="${input.value[0]}">
+                        <input class="input__form" type="${input.typeInput}" id="${input.value[0]}">
                     `
+                    
                     form.appendChild(div);
                 }
                 break;
@@ -97,13 +110,11 @@ function addForm(newForm, title){
     })
 }
 
-addForm(formAddActive, "Formulario de registro de activo"); 
+
 
 //Para agregar valores traidos de las peticiones, se podria traer un objeto y desestructurarlo en variables e ingresarlo a la funcion de mostrar
 
 
-const template = `
 
-`
 //si el data set incluye x cosa, 
 //Procede a buscar en la lista de contenedores 
---
+++ js/dataForm.js
@@ -0,0 +1,64 @@
+export const activeInfo = [
+    {
+        value : ["active-code", "Codigo de la transaccion"], //Id que relaciona al input con el label y el texto
+        typeInput : "number",
+    },
+    {
+        value : ["active-name", "Nombre del activo"], //Id que relaciona al input con el label y el texto
+        typeInput : "text",
+    },
+    {
+        value : ["active-serial", "Numero del serial"], //Id que relaciona al input con el label y el texto
+        typeInput : "text",
+    },
+    {
+        value : ["active-value", "Valor Unitario"], //Id que relaciona al input con el label y el texto
+        typeInput : "number",
+    },
+    {
+        value : ["category-active", "Categoria del activo"], 
+        typeInput : "select",
+    },
+    {
+        value : ["active-type", "Tipo del activo"], 
+        typeInput : "select",
+    },
+    {
+        value : ["active-status", "Estado del activo"], 
+        typeInput : "select",
+    },
+    {
+        value : ["active-brand", "Marca del activo"], 
+        typeInput : "select",
+    },
+    {
+        value: ["add-active-id", "registrar Activo"],
+        typeInput : "submit",
+    }
+];
+
+/*--- informacion de persona ---*/
+export const personInfo = [
+    {
+        value : ["person-identification", "Identificacion"],
+        typeInput : "number"
+    },
+    {
+        value : ["person-name", "Nombre"],
+        typeInput : "text"
+    },
+    {
+        value : ["person-email", "Email"],
+        typeInput : 'email'
+    },
+    {
+        value : ["person-type", "Tipo de persona"],
+        typeInput : 'select'
+    },
+    {
+        value : ["", ""]
+    }
+
+];
+
+
